1bbd777900626088a6f5f0906763080b16fcd1b4
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2021-06-21 22:32:56 +0200

    Service classes and full CRUD implemented

--- src/app/game_components/trainer/trainer.service.ts
+++ src/app/game_components/trainer/trainer.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Trainer } from './trainer';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -10,14 +10,24 @@ import { map } from 'rxjs/operators';
 })
 export class TrainerService {
 
-  private getTrainersEndpoint: string = 'http://localhost:8080/api/trainers';
-
+  private trainersEndpoint: string = 'http://localhost:8080/api/trainers';
+  private httpHeaders: HttpHeaders = new HttpHeaders({'Content-type': 'application/json'});
+  
   constructor(private http: HttpClient) { }
 
   getTrainers(): Observable<Trainer[]> {
-    let response = this.http.get(this.getTrainersEndpoint).pipe(
+    let response = this.http.get(this.trainersEndpoint).pipe(
       map( response => response as Trainer[] )
     );
     return response;
   }
+
+  getTrainer(id: Number): Observable<Trainer> {
+    return this.http.get<Trainer>(`${this.trainersEndpoint}/${id}`);
+  }
+
+  create(trainer: Trainer): Observable<Trainer> {
+    return this.http.post<Trainer>(this.trainersEndpoint, trainer, {headers: this.httpHeaders});
+  }
+
 }
---
+++ src/app/game_components/trainer/trainers.form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainers.form',
+  templateUrl: './trainers.form.component.html',
+})
+export class TrainersFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
