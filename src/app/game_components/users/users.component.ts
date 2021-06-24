a97635912c216b1cfba402501bcecdddfcad0d58
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2021-06-24 14:24:05 +0200

    Error handling on observablescd

--- src/app/game_components/users/users.component.ts
+++ src/app/game_components/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { User } from './user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(
+      users => this.users = users
+    );
+  }
+
+  delete(user: User): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Estás seguro?',
+      text: `¿Seguro que deseas eliminar a ${user.username}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, estoy seguro!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.userService.delete(user.id).subscribe(
+          response => {
+            this.users = this.users.filter(us => us !== user)
+            swalWithBootstrapButtons.fire(
+              'Acción completada!',
+              `${user.username} ha sido eliminado del sistema`,
+              'success'
+            )
+          }
+        )
+      } 
+    })
   }
 
 }
